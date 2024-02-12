04beec2910e04e768de94ebf1002b2f5b69d991b
Dessian-Guiro <135682316+Dessian-Guiro@users.noreply.github.com>
2024-02-12 17:31:30 -0600

    modified all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import "./App.css";
-import { useState } from "react";
-import ContactList from "./components/ContactList";
-import SelectedContact from "./components/SelectedContact";
+import './App.css'
+import ContactDetails from './components/ContactDetails';
+import ContactList from './components/ContactList'
+import { useState, useEffect } from 'react'
 
-export default function App() {
+function App() {
   const [selectedContactId, setSelectedContactId] = useState(null);
+  const [detailedContact, setDetailedContact] = useState(null);
+
+  useEffect(() => {
+    async function fetchSelectedContact() {
+      try {
+        const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/" + selectedContactId);
+        const result = await response.json();
+        setDetailedContact(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    if (selectedContactId) {
+      fetchSelectedContact()
+    }
+  }, [selectedContactId])
+
 
   return (
     <>
-      {selectedContactId ? (
-        <SelectedContact
-        selectedContactId={selectedContactId}
-        setSelectedContactId={setSelectedContactId}
-      />
-      ) : (
-        <ContactList setSelectedContactId={setSelectedContactId}/>
-      )}
+    
+      {detailedContact ? (<ContactDetails detailedContact={detailedContact}/>) : (<ContactList setSelectedContactId={setSelectedContactId} />)}
     </>
-  );
+  )
 }
+export default App
--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -1,14 +1,11 @@
 
-export default function ContactRow({ setSelectedContactId, contact }) {
+export default function ContactRow({ contact, setSelectedContactId }) {
+
     return (
-      <tr
-        onClick={() => {
-          setSelectedContactId(contact.id);
-        }}
-      >
-        <td>{contact.name}</td>
-        <td>{contact.email}</td>
-        <td>{contact.phone}</td>
-      </tr>
-    );
-  }
+        <tr onClick={() => setSelectedContactId(contact.id)}>
+          <td>{contact.name}</td>
+          <td>{contact.email}</td>
+          <td>{contact.phone}</td>
+        </tr>
+      );
+    }
